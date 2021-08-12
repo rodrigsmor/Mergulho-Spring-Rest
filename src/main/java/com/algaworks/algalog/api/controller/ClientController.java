b1bff788a58e45acfe439c402c104101fe380663
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-08-12 18:22:24 -0300

    Modulo 2 Aula 2

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,6 +1,9 @@
 package com.algaworks.algalog.api.controller;
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,21 +11,12 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClientController {
+	@PersistenceContext
+	private EntityManager manager;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var client1 = new Cliente();
-		client1.setId(1L);
-		client1.setName("João");
-		client1.setPhone("(34) 99999-1111");
-		client1.setEmail("joaodascouves@gmail.com");
-		
-		var client2 = new Cliente();
-		client2.setId(2L);
-		client2.setName("Maria");
-		client2.setPhone("(11) 97777-2222");
-		client2.setEmail("Maria.ss@gmail.com");
-		
-		return Arrays.asList(client1, client2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
