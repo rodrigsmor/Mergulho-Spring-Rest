5e309f3dfcf2b9864bae37fbf4d69f438586605e
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-08-13 12:41:54 -0300

    Modulo 3 Aula 1 v2 final

--- src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.model.StatusEntrega;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
 import lombok.AllArgsConstructor;
@@ -14,10 +17,14 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Service
 public class SolicitacaoEntregaService {
+	private CatalogoClienteService catalogoClienteService;
 	private EntregaRepository entregaRepository;
 	
 	@Transactional
 	public Entrega solicitar(Entrega entrega) {
+		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+		
+		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
 		entrega.setDataPedido(LocalDateTime.now());
 		
