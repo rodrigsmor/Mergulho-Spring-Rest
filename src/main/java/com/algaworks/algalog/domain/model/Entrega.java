49565949917767219ae407ba0070b83a86845125
RodrigoMoreiraDaSilva <rodrigomoreiradasilva.of@gmail.com>
2021-08-14 21:28:19 -0300

    Modulo 3 Aula 6 v1

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -2,6 +2,8 @@ package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -11,14 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
-
-import com.algaworks.algalog.domain.ValidationGroups;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import javax.persistence.OneToMany;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -42,6 +37,9 @@ public class Entrega {
 	private Destinatario destinatario;
 	
 	private BigDecimal taxa;
+	
+	@OneToMany(mappedBy = "entrega")
+	private List<Ocorrencia> ocorrencias = new ArrayList<>();
 
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
@@ -49,4 +47,16 @@ public class Entrega {
 	private OffsetDateTime dataPedido;
 
 	private OffsetDateTime dataFinalizacao;
+	
+	public Ocorrencia adicionarOcorrencia(String descricao) {
+		Ocorrencia ocorrencia = new Ocorrencia();
+		
+		ocorrencia.setDescricao(descricao);
+		ocorrencia.setDataRegistro(OffsetDateTime.now());
+		ocorrencia.setEntrega(this);
+		
+		this.getOcorrencias().add(ocorrencia);
+		
+		return ocorrencia;
+	}
 }
